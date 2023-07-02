d24a10dcac61fa699b5a0dab7826a6dfc83f241c
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-07-02 06:32:13 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/konichiwa55115/amirbaghdady /LazyDeveloper
-cd /LazyDeveloper
+git clone https://github.com/konichiwa55115/amirbaghdady /kony
+cd /kony
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 gunicorn app:app & python3 -m forwarder
